871a062d4e2e70ff589d51428ecb29ec2a9cca33
Wbert Adrian Castro Vera <wbertc@gmail.com>
2018-08-19 23:00:23 -0500

    first graphql server

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
-console.log("start!!!")
+import { ApolloServer, gql } from 'apollo-server'
+
+const books = [
+  {
+    title: 'Harry potter and the chambers of secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton'
+  }
+]
+
+const typeDefs = gql`
+  # Comments in GraphQL
+  type Book {
+    title: String,
+    author: String
+  }
+  
+  type Query {
+    books: [Book]
+  }
+`
+
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+}
+
+const server = new ApolloServer({typeDefs, resolvers})
+
+server.listen().then(({url}) => {
+  console.log(`Server ready at ${url}`)
+})
